009f46d43828e1c56a336e9f9770cce63e74774c
Tolfinho <tolfojoao2@gmail.com>
2025-12-07 00:31:08 -0300

    Adiciona .editorconfig e prettier para padronização do código

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Um pequeno passo para o homem, um grande passo na carreira DEV!!!</h1>
+  return (
+    <h1>Um pequeno passo para o homem, um grande passo na carreira DEV!!!</h1>
+  );
 }
 
 export default Home;
